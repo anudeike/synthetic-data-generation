f2c83220b56cef5ea25882e8b1d84cb6e44c6326
Ikechukwu <31142429+anudeike@users.noreply.github.com>
2024-06-14 22:29:33 -0700

    added a new dataset to practice analysis

--- random_sampling_generation.py
+++ random_sampling_generation.py
@@ -1,11 +1,37 @@
 import numpy as np
 import pandas as pd
 
+"""
+For this data: create a model that can accurately predict the grade point average of a student given 
+the following features: gender, race/ethnicity, parental level of education, study habits etc
+
+
+"""
+def calculate_summary_statistics(data):
+
+    pass
+
+def linear_regression(data, columnsToDrop, targetColumn, test_size = 0.2):
+    # define the features and target
+    X = data.drop()
+    pass
 
 def main():
 
-    realData = pd.read_csv('real data\Student_performance_data _.csv')
-    print(realData)
+
+    realData = pd.read_csv('real data\spotify_track_dataset.csv')
+    print(realData.head())
+
+    print(realData.columns)
+
+    # using linear regression as a baseline
+
+    # where there are null values, remove the entire row
+    realData.dropna(inplace = True)
+
+    # encode the categorical varliables (one hot encoding)
+    onehotdata = pd.get_dummies(realData, columns=["album_name", "artists", "track_name", "track_genre"], drop_first=True)
+    print(onehotdata.head())
     pass
 
 main()
